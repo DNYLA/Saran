db1c01e0a2fc1239983e65d6f1d0f2ae893af7e2
Danyaal <Danyaal>
2022-06-22 20:35:26 +0100

    Disabled Steal command

--- src/commands/Moderation/Steal.ts
+++ src/commands/Moderation/Steal.ts
@@ -1,43 +1,40 @@
-import axios from 'axios';
-const cheerio = require('cheerio');
-import {
-  Message,
-  MessageActionRow,
-  MessageButton,
-  MessageEmbed,
-  Util,
-} from 'discord.js';
-import { fetchQueryImages } from '../../api/WebSearch';
-import Command from '../../utils/base/command';
-import DiscordClient from '../../utils/client';
-import {
-  getUserFromId,
-  GetUserFromMessage,
-} from '../../utils/Helpers/Moderation';
+// import axios from 'axios';
+// const cheerio = require('cheerio');
+// import {
+//   Message,
+//   MessageActionRow,
+//   MessageButton,
+//   MessageEmbed,
+//   Util,
+// } from 'discord.js';
+// import { fetchQueryImages } from '../../api/WebSearch';
+// import Command from '../../utils/base/command';
+// import DiscordClient from '../../utils/client';
+// import { GetUserFromMessage } from '../../utils/Helpers/Moderation';
 
-export default class GetAvatar extends Command {
-  constructor() {
-    super('steal', 'Tools', ['']);
-  }
+// export default class GetAvatar extends Command {
+//   constructor() {
+//     super('steal', 'Tools', ['']);
+//   }
 
-  async run(client: DiscordClient, message: Message, args: string[]) {
-    const user = await getUserFromId(client, message, args);
-    if (!user) return;
+//   async run(client: DiscordClient, message: Message, args: string[]) {
+//     const user = await getUserFromId(client, message, args);
+//     if (!user) return;
 
-    message.channel.sendTyping();
-    if (args.length === 0) return message.reply('Send an emote you!');
-    // if (args.length === 0) return message.reply('Provide a URL to scrape!');
-    for (const emojiText of args) {
-      console.log(emojiText);
-      const emoji = Util.parseEmoji(emojiText);
+//     message.channel.sendTyping();
+//     if (args.length === 0) return message.reply('Send an emote you!');
+//     // if (args.length === 0) return message.reply('Provide a URL to scrape!');
+//     for (const emojiText of args) {
+//       console.log(emojiText);
+//       const emoji = Util.parseEmoji(emojiText);
 
-      if (emoji.id) {
-        const type = emoji.animated ? '.gif' : '.png';
-        console.log(emoji.id);
-        const url = `https://cdn.discordapp.com/emojis/${emoji.id + type}`;
-        await message.guild.emojis.create(url, emoji.name);
-        message.reply('Created the emoji');
-      }
-    }
-  }
-}
+//       if (emoji.id) {
+//         const type = emoji.animated ? '.gif' : '.png';
+//         console.log(emoji.id);
+//         const url = `https://cdn.discordapp.com/emojis/${emoji.id + type}`;
+//         await message.guild.emojis.create(url, emoji.name);
+//         message.reply('Created the emoji');
+//       }
+//     }
+//   }
+// }
