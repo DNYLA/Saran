1b882f5379833fde88ad8fd6707ab3c8222afbcf
Dan <22224922+DNYLA@users.noreply.github.com>
2022-07-04 01:57:55 +0100

    Added Loickdown

---
+++ src/commands/Moderation/UnLockdown.ts
@@ -0,0 +1,31 @@
+import { GuildChannel, Message } from 'discord.js';
+import StartTyping from '../../hooks/StartTyping';
+import { MentionIdOrArg } from '../../utils/argsparser';
+import Command, { ArgumentTypes } from '../../utils/base/command';
+import { getGuildMemberFromMention } from '../../utils/Helpers/Moderation';
+
+export default class Mute extends Command {
+  constructor() {
+    super('unlockdown', {
+      requirments: {
+        permissions: {
+          administrator: true,
+        },
+      },
+      invalidPermissions: 'You must be admin to use this!',
+      hooks: {
+        preCommand: StartTyping,
+      },
+    });
+  }
+
+  async run(message: Message) {
+    const channel = (await message.channel.fetch()) as GuildChannel;
+
+    await channel.permissionOverwrites.create(channel.guild.roles.everyone, {
+      SEND_MESSAGES: true,
+    });
+
+    message.reply('The channel has been LOCKED DOWN!');
+  }
+}
