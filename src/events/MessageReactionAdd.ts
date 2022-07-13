ea15c879ffab6669e47890f1cb7b9e8195d35c2a
Dan <22224922+DNYLA@users.noreply.github.com>
2022-07-14 00:18:30 +0100

    Probably fixed sob board

--- src/events/MessageReactionAdd.ts
+++ src/events/MessageReactionAdd.ts
@@ -22,7 +22,6 @@ export default class MessageReactionAdd extends Event {
     const message = await reaction.message.fetch();
 
     const alreadySet = await getReactionBoardInfo(message.id, guild.id);
-    if (!alreadySet) return;
 
     try {
       await message.guild.channels.fetch();
@@ -39,34 +38,81 @@ export default class MessageReactionAdd extends Event {
       );
 
       if (alreadySet) {
-        const preSet = await channel.messages.fetch(alreadySet.embedMessageId);
-        if (preSet) {
-          const messageEmbed = new MessageEmbed()
-            .setColor('#1e7842')
-            .setAuthor({
-              name: message.author.username,
-              url: message.url,
-              iconURL: message.author.avatarURL(),
-            })
-            .setDescription(message.content)
-            .addFields(
-              {
-                name: `#${reactionChannel.name}`,
-                value: `[Go To Message](${message.url})`,
-                inline: true,
-              },
-              {
-                name: 'Emoji',
-                value: `${reaction.emoji.name} x${sobEmoji.count}`,
-              }
-            )
-            .setFooter({ text: 'Emoji Score: 15' })
-            .setTimestamp();
-          preSet.edit({ embeds: [messageEmbed] });
-          await updateReactionBoardInfo(message.id, guild.id, sobEmoji.count);
-          return;
+        try {
+          const preSet = await channel.messages.fetch(
+            alreadySet.embedMessageId
+          );
+          if (preSet) {
+            const messageEmbed = new MessageEmbed()
+              .setColor('#1e7842')
+              .setAuthor({
+                name: message.author.username,
+                url: message.url,
+                iconURL: message.author.avatarURL(),
+              })
+              .setDescription(message.content)
+              .addFields(
+                {
+                  name: `#${reactionChannel.name}`,
+                  value: `[Go To Message](${message.url})`,
+                  inline: true,
+                },
+                {
+                  name: 'Emoji',
+                  value: `${reaction.emoji.name} x${sobEmoji.count}`,
+                }
+              )
+              .setFooter({ text: 'Emoji Score: 15' })
+              .setTimestamp();
+            preSet.edit({ embeds: [messageEmbed] });
+            await updateReactionBoardInfo(message.id, guild.id, sobEmoji.count);
+            return;
+          }
+        } catch (err) {
+          console.log(err);
         }
       }
+
+      if (alreadySet) return;
+      if (!sobEmoji) return;
+      if (sobEmoji.count < config.reactionBoardLimit) return;
+      if (!reaction.message.channel.isText()) return;
+
+      // const attachemnt = reaction.message.attachments.first();
+      // console.log(reaction.message.url);
+      // if (attachemnt) imageUrl =
+
+      // console.log(attachemnt);
+
+      const messageEmbed = new MessageEmbed()
+        .setColor('#1e7842')
+        .setAuthor({
+          name: message.author.username,
+          url: message.url,
+          iconURL: message.author.avatarURL(),
+        })
+        .setDescription(message.content)
+        .addFields(
+          {
+            name: `#${reactionChannel.name}`,
+            value: `[Go To Message](${message.url})`,
+            inline: true,
+          },
+          {
+            name: 'Emoji',
+            value: `${reaction.emoji.name} x${sobEmoji.count}`,
+          }
+        )
+        .setFooter({ text: 'Emoji Score: 15' })
+        .setTimestamp();
+
+      const sentMessage = await channel.send({ embeds: [messageEmbed] });
+      await createReactionBoardInfo(
+        sobEmoji.count,
+        sobEmoji.emoji.name,
+        sentMessage.id,
+        message
+      );
     } catch (err) {
       console.log(err);
       console.log('Caught Error');
