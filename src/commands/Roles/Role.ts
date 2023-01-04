0d1283c3ade5ccf504819d46a66959144dc2f2b3
Dan <22224922+DNYLA@users.noreply.github.com>
2023-01-04 13:17:09 +0000

    Role cmd works with RoleName, id and mention

--- src/commands/Roles/Role.ts
+++ src/commands/Roles/Role.ts
@@ -1,6 +1,10 @@
 import { Message } from 'discord.js';
 import StartTyping from '../../hooks/StartTyping';
-import { MentionUserId, SelfUserId } from '../../utils/argsparser';
+import {
+  MentionUserId,
+  RoleMentionIdOrArg,
+  SelfUserId,
+} from '../../utils/argsparser';
 import Command, { ArgumentTypes } from '../../utils/base/command';
 
 export default class RoleCommand extends Command {
@@ -23,6 +27,7 @@ export default class RoleCommand extends Command {
           type: ArgumentTypes.SINGLE,
         },
         {
+          parse: RoleMentionIdOrArg,
           name: 'roleId',
           type: ArgumentTypes.SINGLE,
         },
@@ -31,17 +36,34 @@ export default class RoleCommand extends Command {
   }
 
   async run(message: Message, args: { targetUserId: string; roleId: string }) {
+    console.log(args.roleId);
+
     try {
-      const role = await message.guild.roles.fetch(args.roleId);
+      let role = await message.guild.roles.fetch(args.roleId);
       const user = await message.guild.members.fetch(args.targetUserId);
       await user.fetch();
 
-      if (user.roles.cache.has(role.id)) {
-        user.roles.remove(role);
+      if (!role) {
+        const serverRoles = await message.guild.roles.fetch();
+        serverRoles.map((fetchedRole) => {
+          console.log(`${fetchedRole.name} : ${args.roleId.toLowerCase()}`);
+          if (fetchedRole.name.toLowerCase() === args.roleId.toLowerCase())
+            role = fetchedRole;
+        });
+      }
+      if (!role) return message.reply('Unable to find role!');
+      const hasRole = user.roles.cache.has(role.id);
+
+      if (hasRole) {
+        await user.roles.remove(role);
       } else user.roles.add(role);
+      return message.reply(
+        `Successfully ${hasRole ? 'removed' : 'added'} ${role.name} ${
+          hasRole ? 'from' : 'to'
+        } ${user.displayName}`
+      );
     } catch (err) {
-      return message.reply('idk an error happened');
+      return message.reply('Unable to give user Role');
     }
-    return message.reply('Role');
   }
 }
