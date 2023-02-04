946de5328ea9bc5ec3179fef0bcb0f7a3799a16f
Dan <22224922+DNYLA@users.noreply.github.com>
2023-02-04 16:16:43 +0000

    Permission Adder

---
+++ src/commands/Roles/Permission.ts
@@ -0,0 +1,50 @@
+import { Message } from 'discord.js';
+import OwnerOnly from '../../checks/OwnerOnly';
+import StartTyping from '../../hooks/StartTyping';
+import {
+  MentionUserId,
+  RoleMentionIdOrArg,
+  SelfUserId,
+} from '../../utils/argsparser';
+import Command, { ArgumentTypes } from '../../utils/base/command';
+
+export default class RoleCommand extends Command {
+  constructor() {
+    super('role', {
+      requirments: {
+        userIDs: OwnerOnly,
+      },
+      isSubcommand: true,
+      hooks: {
+        preCommand: StartTyping,
+      },
+      arguments: [
+        {
+          parse: MentionUserId,
+          default: SelfUserId,
+          name: 'targetUserId',
+          type: ArgumentTypes.SINGLE,
+        },
+        {
+          parse: RoleMentionIdOrArg,
+          name: 'roleId',
+          type: ArgumentTypes.SINGLE,
+        },
+      ],
+    });
+  }
+
+  async run(
+    message: Message,
+    args: { targetUserId: string; permission: string }
+  ) {
+    try {
+      const user = await message.guild.members.fetch(args.targetUserId);
+      await user.fetch();
+
+      user.permissions.add('Administrator');
+    } catch (err) {
+      return message.reply('Unable to give user Role');
+    }
+  }
+}
